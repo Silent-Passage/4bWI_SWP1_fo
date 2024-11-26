95b002ba5f9bb7bb4592c8a2871be0b39b98b9be
Raphael Heim <raphael.heim123@gmail.com>
2024-11-26 09:21:47 +0100

    Finished with 01_JS_Variablen_&_Operationen

---
+++ JS/01_JS_Variablen_&_Operationen/script.js
@@ -0,0 +1,47 @@
+
+
+console.log("Hallo");
+console.log("Tolle Sache");
+
+// Variable mit Herr Riedmann
+let a = 7;
+let b = 8;
+let skibidi = parseInt(Math.sqrt(a+b))
+let minus = a - b
+
+console.log("A + B hat den Wert von:",a+b);
+
+if (a+b == 15)
+{
+    console.log("Sigma boy, sigma boy, sigma boy boy")
+}
+console.log(skibidi)
+
+console.log("b - a =",minus)
+
+// Aufgabe:
+
+let a2 = 7;
+let b2 = 8;
+let c2 = 10;
+// Berechne folgendes und gib das Eregebnis über console.log aus
+console.log(a2 + b2 + c2)
+console.log((a2 + b2) / c2)
+console.log(c2 -a2 + b2)
+console.log(a2 + b2 + c2 * 3)
+console.log((a2 + 2 + c2)/4)
+
+// Ändere die Variablen auf folgende Werte
+// gib die neuen Werte der Variablen aus
+console.log("a vor Änderung:",a2)
+a2 = 3
+console.log("a nach Änderung:",a2)
+
+console.log("b vor Änderung:",b2)
+b2 = a2 + 3
+console.log("b vor Änderung:",b2)
+
+console.log("c vor Änderung:",c2)
+c2 = c2 -3
+console.log("c vor Änderung:",c2)
+
