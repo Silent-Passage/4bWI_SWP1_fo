2b759c99ff915971ff61d37068170729dce6fbb6
Raphael Heim <raphael.heim123@gmail.com>
2024-11-26 09:32:12 +0100

    Finished with 02_JS_Strings_&_Zahlen

--- JS/02_JS_Strings_&_Zahlen/script.js
+++ JS/02_JS_Strings_&_Zahlen/script.js
@@ -1,1 +1,15 @@
+//1) Berechne das Ergebnis von 17.4 / 3.9. Gib folgenden Satz aus:
+//The result of 17.4 divided by 3.9 is 4.461538461538462
 
+let Brainrot = 17.4;
+let David = 3.9;
+
+console.log("The result of",Brainrot,"divided by",David,"is",Brainrot/David)
+
+//2) Lege eine Variable firstname und eine Varialbe lastname an. Weise den Variablen Deinen Namen zu. Gib folgendes aus:
+//"Ich heiße VORNAME NACHNAME"
+
+let Mein_Vorname = "Raphael";
+let Mein_Nachname = "Heim"
+
+console.log("Ich heiße",Mein_Vorname,Mein_Nachname)
