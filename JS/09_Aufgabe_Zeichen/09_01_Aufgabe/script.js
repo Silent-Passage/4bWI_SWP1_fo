ab11688e8f2083fb8186dd65f0199559ff605167
Raphael Heim <raphael.heim123@gmail.com>
2024-12-06 16:26:43 +0100

    Starting with 09_01_Aufgabe

---
+++ JS/09_Aufgabe_Zeichen/09_01_Aufgabe/script.js
@@ -0,0 +1,45 @@
+// Write a program that prints the numbers from 1 to 222
+// except for multiples of 5 where you have to print "Foo"
+// and for the multiples of 7 where you have to print "Bar".
+// For numbers which are multiples of both 5 and 7 print "FooBar".
+
+// Input
+// 36
+
+// Output
+// 1
+// 2
+// 3
+// 4
+// Foo
+// 6
+// Bar
+// 8
+// 9
+// Foo
+// 11
+// 12
+// 13
+// Bar
+// Foo
+// 16
+// 17
+// 18
+// 19
+// Foo
+// Bar
+// 22
+// 23
+// 24
+// Foo
+// 26
+// 27
+// Bar
+// 29
+// Foo
+// 31
+// 32
+// 33
+// 34
+// FooBar
+// 36
