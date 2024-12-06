5df58520c2df8bbf68b7766664e49a16c7ffada8
Raphael Heim <raphael.heim123@gmail.com>
2024-12-06 16:04:27 +0100

    Finished with 07_01_Aufgabe

--- JS/07_Logische_Operatoren/07_01_Aufgabe/script.js
+++ JS/07_Logische_Operatoren/07_01_Aufgabe/script.js
@@ -10,3 +10,36 @@
 // 1 2 3 4
 // Output
 // 6
+
+let N1 = Math.random() * 100;
+N1_random = Math.floor(N1);
+let N2 = Math.random() * 100;
+N1_random = Math.floor(N2);
+let N3 = Math.random() * 100;
+N1_random = Math.floor(N3);
+let N4 = Math.random() * 100;
+N1_random = Math.floor(N4);
+
+let count = 0;
+console.log("N1:",Math.floor(N1));
+console.log("N2:",Math.floor(N2));
+console.log("N3:",Math.floor(N3));
+console.log("N4:",Math.floor(N4));
+
+if(Math.floor(N1)%2==0)
+{
+    count+=Math.floor(N1)
+}
+if(Math.floor(N2)%2==0)
+{
+    count+=Math.floor(N2)
+}
+if(Math.floor(N3)%2==0)
+{
+    count+=Math.floor(N3)
+}
+if(Math.floor(N4)%2==0)
+{
+    count+=Math.floor(N4)
+}
+console.log(count);
