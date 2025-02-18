f768281fc9b1737f318a5948ac1137ddc3de6bf4
Raphael Heim <raphael.heim123@gmail.com>
2025-02-18 09:29:04 +0100

    Mid Push 03_JSON_um_sich_Daten_zu_merken

---
+++ HTML and JS/03_JSON_um_sich_Daten_zu_merken/script.js
@@ -0,0 +1,19 @@
+let data = [
+  { firstName: "David", lastName: "Bischof", points: 1 },
+  { firstName: "Peter", lastName: "Gstreu", points: 30 },
+  { firstName: "Raphael", lastName: "Heim", points: 100 },
+];
+
+// for (let i = 0; i < data.length; i++) {
+//   const element = data[i];
+//   console.log(element.firstName);
+// }
+
+function loadpeople() {
+  let html = "";
+  data.forEach((element) => {
+    html += "<div>" + element.firstName + " " + element.lastName + "</div>";
+  });
+  document.getElementById("content").innerHtml = html;
+}
+loadpeople();
