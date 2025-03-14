da58a4be3bdd389bcc06b1d72a6ac5e0a90e21bc
Raphael Heim <raphael.heim123@gmail.com>
2025-03-14 15:56:57 +0100

    Finished with 03_Demo_Projekt_Cards_und_Daten

--- REACT/03_Demo_Projekt_Cards_und_Daten/src/App.jsx
+++ REACT/03_Demo_Projekt_Cards_und_Daten/src/App.jsx
@@ -2,11 +2,18 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import PeopleContainer from "./components/peoplecontainer";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <>
+      <div>
+        <PeopleContainer />
+      </div>
+    </>
+  );
 }
 
 export default App;
