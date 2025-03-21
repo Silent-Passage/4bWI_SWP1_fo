94c7997118843b4ebe1ef72aeaf5377101f5f2b4
Raphael Heim <raphael.heim123@gmail.com>
2025-03-21 16:22:25 +0100

    Mid Push 05_Demo_Filter

--- REACT/05_Demo_Filter/src/components/peoplecontainer.jsx
+++ REACT/05_Demo_Filter/src/components/peoplecontainer.jsx
@@ -3,29 +3,60 @@ import Card from "./card";
 
 export default function PeopleContainer() {
   const [people, setPeople] = useState([]);
-  const [isActive, setIsActive] = useState(false);
+  const [filteredName, setFilteredName] = useState([]);
+  const [filteredTitle, setFilteredTitle] = useState([]);
 
   useEffect(() => {
-    fetch("https://67d408d38bca322cc26bf6e0.mockapi.io/person")
-      .then((response) => response.json())
-      .then((json) => {
-        console.log(json);
-        setPeople(json);
-      });
+    fetch("https://67d408d38bca322cc26bf6e0.mockapi.io/person").then((res) =>
+      res.json().then((data) => {
+        console.log(data);
+        setPeople(data);
+        setFilteredName(data);
+        setFilteredTitle(data);
+      })
+    );
   }, []);
 
+  const filterName = (filter) => {
+    let namefiltered = people.filter((person) => person.name.includes(filter));
+    setFilteredName(namefiltered);
+  };
+  const filterTitle = (filter) => {
+    let titlefiltered = people.filter((person) =>
+      person.title.includes(filter)
+    );
+    setFilteredTitle(titlefiltered);
+  };
   return (
-    <div>
-      <h1>People</h1>
-      <button
-        onClick={() => {
-          setColor("orange");
-        }}
-      >
-        Change Color
-      </button>
-      <div className="grid grid-cols-2 gap-4 w-full lg:grid-cols-4">
-        {people.map((person) => {
+    <div className="">
+      <div className="fixed border mb-4 bg-gray-500 w-full h-36 pl-8 pt-5">
+        <h1>
+          Name
+          <input
+            className="border p-4 bg-white"
+            type="text"
+            placeholder="Search..."
+            onChange={(el) => {
+              console.log(el.target.value);
+              filterName(el.target.value);
+            }}
+          />
+        </h1>
+        <h1>
+          Title
+          <input
+            className="border p-4 bg-white"
+            type="text"
+            placeholder="Search..."
+            onChange={(el) => {
+              console.log(el.target.value);
+              filterTitle(el.target.value);
+            }}
+          />
+        </h1>
+      </div>
+      <div className="grid grid-cols-2 gap-4 w-full lg:grid-cols-4 p-8 pt-40">
+        {filteredName.map((person) => {
           return (
             <div>
               <Card
