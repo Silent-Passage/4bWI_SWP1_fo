658f985ee1464f800cf0e1bf3271f42b377294e5
Raphael Heim <raphael.heim123@gmail.com>
2025-03-21 15:23:59 +0100

    Starting with 05_Demo_Filter

---
+++ REACT/05_Demo_Filter/src/components/peoplecontainer.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import Card from "./card";
+
+export default function PeopleContainer() {
+  const [people, setPeople] = useState([]);
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    fetch("https://67d408d38bca322cc26bf6e0.mockapi.io/person")
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+        setPeople(json);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>People</h1>
+      <button
+        onClick={() => {
+          setColor("orange");
+        }}
+      >
+        Change Color
+      </button>
+      <div className="grid grid-cols-2 gap-4 w-full lg:grid-cols-4">
+        {people.map((person) => {
+          return (
+            <div>
+              <Card
+                name={person.name}
+                title={person.jobtitle}
+                img={"https://picsum.photos/100/"} //Cloudfare ist down
+              />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
