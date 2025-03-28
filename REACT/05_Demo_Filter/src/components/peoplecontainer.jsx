f7899054f01424933ff1bbc19dd08afa0b39ec78
Raphael Heim <raphael.heim123@gmail.com>
2025-03-28 15:20:46 +0100

    Finished with 05_Demo_Filter

--- REACT/05_Demo_Filter/src/components/peoplecontainer.jsx
+++ REACT/05_Demo_Filter/src/components/peoplecontainer.jsx
@@ -3,70 +3,62 @@ import Card from "./card";
 
 export default function PeopleContainer() {
   const [people, setPeople] = useState([]);
-  const [filteredName, setFilteredName] = useState([]);
-  const [filteredTitle, setFilteredTitle] = useState([]);
+  const [filteredPeople, setFilteredPeople] = useState([]);
+  const [searchName, setSearchName] = useState("");
+  const [searchTitle, setSearchTitle] = useState("");
 
   useEffect(() => {
-    fetch("https://67d408d38bca322cc26bf6e0.mockapi.io/person").then((res) =>
-      res.json().then((data) => {
-        console.log(data);
+    fetch("https://67d408d38bca322cc26bf6e0.mockapi.io/person")
+      .then((res) => res.json())
+      .then((data) => {
         setPeople(data);
-        setFilteredName(data);
-        setFilteredTitle(data);
-      })
-    );
+        setFilteredPeople(data);
+      });
   }, []);
 
-  const filterName = (filter) => {
-    let namefiltered = people.filter((person) => person.name.includes(filter));
-    setFilteredName(namefiltered);
-  };
-  const filterTitle = (filter) => {
-    let titlefiltered = people.filter((person) =>
-      person.title.includes(filter)
+  useEffect(() => {
+    let filtered = people.filter(
+      (person) =>
+        person.name.toLowerCase().includes(searchName.toLowerCase()) &&
+        person.jobtitle &&
+        person.jobtitle.toLowerCase().includes(searchTitle.toLowerCase())
     );
-    setFilteredTitle(titlefiltered);
-  };
+    setFilteredPeople(filtered);
+  }, [searchName, searchTitle, people]);
+
   return (
-    <div className="">
-      <div className="fixed border mb-4 bg-gray-500 w-full h-36 pl-8 pt-5">
-        <h1>
-          Name
+    <div>
+      <div className="fixed border mb-5 bg-gray-500 w-full h-30 pl-8 flex flex-row gap-4 items-center">
+        <div>
+          <h1>Name</h1>
           <input
-            className="border p-4 bg-white"
+            className="border p-2 bg-white"
             type="text"
             placeholder="Search..."
-            onChange={(el) => {
-              console.log(el.target.value);
-              filterName(el.target.value);
-            }}
+            value={searchName}
+            onChange={(e) => setSearchName(e.target.value)}
           />
-        </h1>
-        <h1>
-          Title
+        </div>
+        <div>
+          <h1>Job Title</h1>
           <input
-            className="border p-4 bg-white"
+            className="border p-2 bg-white"
             type="text"
             placeholder="Search..."
-            onChange={(el) => {
-              console.log(el.target.value);
-              filterTitle(el.target.value);
-            }}
+            value={searchTitle}
+            onChange={(e) => setSearchTitle(e.target.value)}
           />
-        </h1>
+        </div>
       </div>
       <div className="grid grid-cols-2 gap-4 w-full lg:grid-cols-4 p-8 pt-40">
-        {filteredName.map((person) => {
-          return (
-            <div>
-              <Card
-                name={person.name}
-                title={person.jobtitle}
-                img={"https://picsum.photos/100/"} //Cloudfare ist down
-              />
-            </div>
-          );
-        })}
+        {filteredPeople.map((person) => (
+          <Card
+            key={person.id}
+            name={person.name}
+            title={person.jobtitle}
+            img={"https://picsum.photos/100/"}
+          />
+        ))}
       </div>
     </div>
   );
